ebf61ff3fd0bccae4832e35b3734f60ce61b1b3b
Wangki Lee <lwk2007@naver.com>
2020-11-26 19:05:44 +0900

    security 작업중

---
+++ src/main/java/me/toy/practice/infra/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package me.toy.practice.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers("/login").permitAll()
+                .mvcMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        http
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
+}
--- src/main/java/me/toy/practice/modules/account/AccountService.java
+++ src/main/java/me/toy/practice/modules/account/AccountService.java
@@ -1,15 +1,23 @@
 package me.toy.practice.modules.account;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class AccountService {
+public class AccountService implements UserDetailsService {
 
     private final AccountRepository accountRepository;
 
     public Long saveAccount(AccountSaveDto saveDto) {
         return accountRepository.save(saveDto.toEntity()).getId();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
     }
 }
